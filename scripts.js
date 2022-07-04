5a5660436d03de5433ae9acc4b3dc29350c0b416
Paul Pessoa <paulmspessoa@gmail.com>
2022-07-04 12:33:26 -0300

    bug ngMessages tinha 3 "sss"

---
+++ scripts.js
@@ -0,0 +1,68 @@
+(function (angular) {
+  'use strict';
+  var app = angular.module("listaTelefonica", ['ngMessages']);
+  app
+    .controller("listaTelefonicaCtrl", function ($scope) {
+      $scope.app = "Lista Telefonica";
+      $scope.contatos = [
+        {
+          nome: "Pedro",
+          telefone: "8199559765",
+          operadora: { nome: "Oi" },
+          cor: "Blue",
+        },
+        {
+          nome: "Ana",
+          telefone: "8199550000",
+          operadora: { nome: "Claro" },
+          cor: "purple",
+        },
+        {
+          nome: "Joao",
+          telefone: "8199559764",
+          operadora: { nome: "Vivo" },
+          cor: "green",
+        },
+      ];
+      $scope.operadoras = [
+        { nome: "Oi", codigo: 14, categoria: "Celular" },
+        { nome: "Vivo", codigo: 15, categoria: "Celular" },
+        { nome: "Tim", codigo: 16, categoria: "Fixo" },
+        { nome: "Claro", codigo: 17, categoria: "Fixo" },
+      ];
+      $scope.cores = [
+        { nome: "Azul", valor: "blue" },
+        { nome: "Verde", valor: "green" },
+        { nome: "Vermelho", valor: "red" },
+        { nome: "Amarelo", valor: "yellow" },
+      ];
+      $scope.adicionarContato = function (contato) {
+        $scope.contatos.push(
+          angular.copy(contato)
+          //    nome: $scope.nome,
+          //    telefone: $scope.telefone,
+        );
+        delete $scope.contato;
+        $scope.contatoForm.$setPristine()
+      };
+      $scope.classe1 = "selecionado";
+      $scope.classe2 = "negrito";
+      $scope.apagarContato = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+          if (!contato.selecionado) return contato;
+        });
+      };
+      $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+          return contato.selecionado;
+        });
+        console.log(isContatoSelecionado);
+      };
+    });
+})(window.angular);
+
+/*
+Copyright 2022 Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
